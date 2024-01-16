9606fb1d6ce4cad2af742a396d7e59fe3c70b48c
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-01-16 12:05:29 +0530

    Corrected code for AM-PM

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateClock() {
   const minutes = date.getMinutes().toString().padStart(2, 0);
   const seconds = date.getSeconds().toString().padStart(2, 0);
 
-  let amOrPm = hours <= 12 ? "AM" : "PM";
+  let amOrPm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12 || 12;
   hours = hours.toString().padStart(2, 0);
   const time = `${hours}:${minutes}:${seconds} ${amOrPm}`;
